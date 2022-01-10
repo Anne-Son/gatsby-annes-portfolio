8b3b32f53d210923ff57711a4f1fc93dc6366c8c
anne-son <slhyuny@gmail.com>
2022-01-09 23:17:24 -0800

    First commit

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+import PropTypes from "prop-types"
+import { Link } from "gatsby"
+
+const Footer = ({ siteTitle }) => (
+  <footer>
+    <h2>Footer</h2>
+  </footer>
+)
+
+
+export default Footer
